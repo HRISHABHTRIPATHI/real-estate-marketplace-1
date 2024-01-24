afa5db35f204ef07b6ef486ef5fd8465e74c527b
Anurag Yadav <anuragyadav20062002@gmail.com>
2024-01-24 20:17:00 +0530

    Added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react"
+/* eslint-disable no-unused-vars */
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
 
 export default function App() {
   return (
-    <h1>
-      Hello
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/sign-in' element={<Signin />}/>
+        <Route path='/sign-up' element={<SignUp />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='profile' element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
