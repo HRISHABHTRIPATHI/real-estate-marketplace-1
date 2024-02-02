c351165173cff8f15d7307f0f63a11cd53266fd2
Anurag Yadav <anuragyadav20062002@gmail.com>
2024-02-02 21:25:35 +0530

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,22 @@
 import express from 'express'
-import { signup } from '../controllers/auth.controller.js';
+import { signup, google,signout,signin } from '../controllers/auth.controller.js';
 
+// Create a new Express router
 const router = express.Router();
 
-router.post('/signup', signup)
+// Define POST routes for authentication
 
-export default router
+// Route for user registration (signup)
+router.post("/signup", signup);
+
+// Route for user authentication (signin)
+router.post("/signin", signin);
+
+// Route for user authentication using Google (Continue with Google)
+router.post("/google", google);
+
+// Route for user sign out
+router.get("/signout", signout);
+
+// Export the router to be used in your main Express application
+export default router;
