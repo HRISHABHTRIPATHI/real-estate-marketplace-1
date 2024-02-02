f477218955c4c5f764fec403785810819475a90e
Anurag Yadav <anuragyadav20062002@gmail.com>
2024-02-02 21:21:03 +0530

    added controllers,middleware,routes and utils primarily

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import authRouter from './routes/auth.route.js'
+import userRouter from './routes/user.route.js'
 
 dotenv.config()
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect('mongodb+srv://anuragyadav20602:6x04puMsaKee7NCf@cluster0.thoou2u.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log("MongoDB Connected")
 }).catch((err)=>{
     console.log(err)
@@ -11,7 +13,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 
 
 const app = express()
+app.use(express.json())
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
+
+app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter)
